02d399266b131102208bf0e8348c07b66f82de5d
amjadsh92 <amjadsharafeddien@gmail.com>
2025-02-26 14:46:09 +0200

    implement colors in quoteBox

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -7,10 +7,31 @@ let allQuotes;
 
 function App() {
 
-  const [quote, setQuote] = useState(null)
+  const [quoteBox, setQuote] = useState({backgroundColor:"bg-primary",quoteTextColor:"text-primary"  })
+  const [color, setColor] = useState({backgroundColor:"bg-primary", textColor:"text-primary"})
   const [loading, setLoading] = useState(true);
   
- 
+  const backgroundColors = [
+    "bg-sunset-orange",
+    "bg-deep-coffee",
+    "bg-emerald-green",
+    "bg-royal-purple",
+    "bg-navy-storm",
+    "bg-mulberry-brown",
+    "bg-coral-red"
+  ];
+  
+  const textColors = [
+    "text-sunset-orange",
+    "text-deep-coffee",
+    "text-emerald-green",
+    "text-royal-purple",
+    "text-navy-storm",
+    "text-mulberry-brown",
+    "text-coral-red"
+  ];
+
+  
   const fetchData = async () => {
 
         
@@ -19,9 +40,13 @@ function App() {
       allQuotes = await response.json();
       allQuotes= allQuotes.quotes
       let ind = Math.floor(Math.random() * 100);
-      let choosedQuote = allQuotes ? allQuotes[ind]?.quote : ""
+      let quote = allQuotes ? allQuotes[ind]?.quote : ""
       let author = allQuotes ? allQuotes[ind]?.author : ""
-      setQuote({choosedQuote, author})
+      let indColor = Math.floor(Math.random() * 7);
+      quoteBox.backgroundColor = backgroundColors[indColor];
+      quoteBox.quoteTextColor = textColors[indColor];
+      setQuote({quote, author, backgroundColor:quoteBox.backgroundColor, quoteTextColor:quoteBox.quoteTextColor })
+      
       return allQuotes
       
       }
@@ -36,9 +61,9 @@ function App() {
   useEffect(()=> {
        
    
-  //(async ()=> {allQuotes = await fetchData()})();
+  
   fetchData() 
-  console.log("allQuotes",allQuotes)
+
 
     }, [])
 
@@ -46,26 +71,28 @@ function App() {
     debugger;
     
     let ind = Math.floor(Math.random() * 100);
-    //allQuotes = await fetchData();
-    let choosedQuote = allQuotes ? allQuotes[ind]?.quote : "undefined";
+    let quote = allQuotes ? allQuotes[ind]?.quote : "undefined";
     let author = allQuotes ? allQuotes[ind]?.author : "undefined";
-    setQuote({choosedQuote, author});
+    let indColor = Math.floor(Math.random() * 7);
+    quoteBox.backgroundColor = backgroundColors[indColor];
+    quoteBox.quoteTextColor = textColors[indColor];
+    setQuote({quote, author, backgroundColor:quoteBox.backgroundColor, quoteTextColor:quoteBox.quoteTextColor });
 
   }  
 
 
     return (
-        <div id="quote-box" className="quote-box border border-dark w-500px h-250px mx-auto">
+        <div id="quote-box" className={`quote-box bg-white border border-dark w-500px mx-auto`}>
           
-          <p id="text" className="text-center"><span>""</span>{quote?.choosedQuote}</p>
-          <p id="author" className="text-end">{quote?.author} </p>
+          <p id="text" className={`text-center ${quoteBox.quoteTextColor}`}><span>""</span>{quoteBox.quote}</p>
+          <p id="author" className={`text-end ${quoteBox.quoteTextColor}`}>{quoteBox.author} </p>
           <div id="change-quote" className = "d-flex justify-content-between align-items-center mt-5 mb-3">
-            <div >
-            <button className="btn btn-primary">twitter</button>
-            <button className="btn btn-primary mx-1">Tumbler</button>
+            <div className="d-flex gap-1">
+            <button className={`btn btn-primary ${quoteBox.backgroundColor} border-0`}>twitter</button>
+            <button className={`btn btn-primary ${quoteBox.backgroundColor} border-0`}>Tumbler</button>
             </div>
-            
-            <button className="btn btn-primary" onClick={changeQuote}>new quote</button>
+                                    
+            <button className={`btn btn-primary ${quoteBox.backgroundColor} border-0`} onClick={changeQuote}>new quote</button>
             
 
 
@@ -81,13 +108,5 @@ function App() {
 
 }     
      
-
-  
-
-
-
-  
-  
-
 
 export default App
