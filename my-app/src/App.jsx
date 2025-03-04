21488948f83586194a6ef06054ca9c2ce4c59708
amjadsh92 <amjadsharafeddien@gmail.com>
2025-03-04 14:55:16 +0200

    Add quote in twitter or tumblre

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -128,8 +128,17 @@ function QuoteBox({updateBackground}){
           <p id="author" className={`text-end ${quoteBox.quoteTextColor} ${isFading ? "fade-out":"fade-in" }`}><span className="me-1">-</span>{quoteBox.author} </p>
          <div id="change-quote" className = "d-flex justify-content-between align-items-center mt-5 mb-3">
             <div className="d-flex gap-1">
-            <button className={`btn btn-primary fade-button ${quoteBox.backgroundColor} btn-hover-light  border-0 twitter`}><FontAwesomeIcon icon={faTwitter} /></button>
-            <button className={`btn btn-primary fade-button ${quoteBox.backgroundColor} btn-hover-light border-0 tumblr`}><FontAwesomeIcon icon={faTumblr} /></button>
+            {/* <a 
+              href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(quoteBox.quote + " - " + quoteBox.author)}`} 
+              target="_blank" 
+              rel="noopener noreferrer"
+              onClick={(event) => event.stopPropagation()} // Prevent triggering other button behavior
+              className={`btn btn-primary fade-button ${quoteBox.backgroundColor} btn-hover-light border-0 twitter`}
+>
+             <FontAwesomeIcon icon={faTwitter} />
+             </a> */}
+             <a href={`https://twitter.com/intent/tweet?${encodeURIComponent(quoteBox.quote + " - " + quoteBox.author)}` } target="_blank"><button className={`btn btn-primary fade-button ${quoteBox.backgroundColor} btn-hover-light border-0 tumblr`}><FontAwesomeIcon icon={faTwitter} /></button></a> 
+            <a href={`https://www.tumblr.com/login?redirect_to=https%3A%2F%2Fwww.tumblr.com%2Fwidgets%2Fshare%2Ftool%3Fposttype%3Dquote%26tags%3Dquotes%252Cfreecodecamp%26caption%3DKevin%2BKruse%26content%3DLife%2Bisn%25E2%2580%2599t%2Babout%2Bgetting%2Band%2Bhaving%252C%2Bit%25E2%2580%2599s%2Babout%2Bgiving%2Band%2Bbeing.%26canonicalUrl%3Dhttps%253A%252F%252Fwww.tumblr.com%252Fbuttons%26shareSource%3Dtumblr_share_button`} target = "_blank"><button className={`btn btn-primary fade-button ${quoteBox.backgroundColor} btn-hover-light border-0 tumblr`}><FontAwesomeIcon icon={faTumblr} /></button></a>
             </div>
                                     
             <button className={`btn btn-primary fade-button ${quoteBox.backgroundColor} btn-hover-light border-0`} onClick={changeQuote}>new quote</button>
